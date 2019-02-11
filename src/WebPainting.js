d69d3e1855b8314f6b2cb821182269247b213b6e
kirisakku <kirisakku@yahoo.co.jp>
2019-02-11 20:21:30 +0900

    鉛筆機能を実装

--- src/WebPainting.js
+++ src/WebPainting.js
@@ -1,28 +1,74 @@
+import Point from "./Point.js";
+
 'use strict';
 
 const LENGTH = 600;
 
 export default class WebPainting {
 
+    constructor() {
+        // パス保持配列
+        this._pathList = [];
+
+        // canvas保持
+        this._ctx = null;
+    }
+
     init(){
         const canvas = document.getElementById('webPainting');
+        canvas.focus();
         this._ctx = canvas.getContext('2d');
-//        canvas.thisArg = this;
+        canvas.thisArg = this;
 
         // イベントハンドラ追加
-        canvas.addEventListener('mousedown', this.mouseDown, false);
-        canvas.addEventListener('dragstart', this.dragStart, false);
-
+        canvas.addEventListener('dragstart', this.dragStart);
+        canvas.addEventListener('drag', this.drag);
+        canvas.addEventListener('dragend', this.dragEnd);
         this._ctx.fillStyle = 'white';
         this._ctx.fillRect(0, 0, 600, 600);
     }
 
-    mouseDown(event) {
-        console.log('mouseDown');
-    }
-
     dragStart(event) {
         console.log('dragStart');
+        const webPainting = this.thisArg;
+
+        // ドラッグ開始地点の座標を追加する
+        const startPoint = new Point(event.offsetX, event.offsetY);
+        webPainting._pathList.push([startPoint]);
+
+        const ctx = webPainting._ctx;
+        // ペンを開始地点に持っていく
+        ctx.save();
+        ctx.beginPath();
+        ctx.strokeStyle = 'black';
+        ctx.moveTo(startPoint.x, startPoint.y);
+    }
+
+    drag(event) {
+        console.log('drag');
+        const webPainting = this.thisArg;
+
+        // ドラッグ中の座標を追加
+        const currentPoint = new Point(event.offsetX, event.offsetY);
+
+        // 何故かdrag完了時にこの座標になっている。マジックナンバー？[要調査]
+        if (currentPoint.x === -29 || currentPoint.y === -8) {
+            return;
+        }
+
+        webPainting._pathList[webPainting._pathList.length - 1].push(currentPoint);
+
+        const ctx = webPainting._ctx;
+        ctx.lineTo(currentPoint.x, currentPoint.y);
+        ctx.stroke();
+    }
+
+    dragEnd(event) {
+        console.log('dragEnd');
+        const webPainting = this.thisArg;
+
+        const ctx = webPainting._ctx;
+        ctx.restore();
     }
 }  
 
